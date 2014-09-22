62ba1ae57040d4599a73ed45afbc90161a202b3f
Oliver4242 <oliver.duerr@gmail.com>
2014-09-22 17:51:26 +0200

    comments

--- R/CreateArbeitsblatt.R
+++ R/CreateArbeitsblatt.R
@@ -16,6 +16,11 @@ createAB.old <- function(infile) {
   inc = includes(before_body = before_body, in_header = header_lsg) 
   lsg <- TRUE
   output_file_base = basename(file_path_sans_ext(infile))
+  
+  # What happens under the hut?
+  # render is from the rmarkdown packages
+  # It takes the rmd and creates an md-file with knitr
+  # Then it takes the md-file and creates an output 
   render(input = infile, pdf_document(includes = inc), output_file = paste0(output_file_base, "_lsg.pdf"), encoding = "UTF-8")
   lsg <- FALSE
   inc = includes(before_body = before_body, in_header = header_nolsg)
