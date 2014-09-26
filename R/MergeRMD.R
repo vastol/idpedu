1ee65b49c5a2233f9fad2de7986eb3994fcbc808
unknown <tolk@CLT-DSK-T-7218.zhaw.ch>
2014-09-26 14:55:58 +0200

    Final mergeRMD command

--- R/MergeRMD.R
+++ R/MergeRMD.R
@@ -13,13 +13,13 @@
 #'
 #' @param dir a temporary working directort
 #' @param title of the exercise sheet
-#' @param files a list of files containing the rmd-files which will be merged
+#' @param files a list of files containing the rmd-files which will be merged. Should be provided as c("file path1","file path2", ...)
 #' 
 #'  To create a list of files with names following some pattern one can use.(pattern=regular expression)
 #'  files = list.files(path='V:/tolk/Private/PROJECT 02 O. DUERR/TEST FILES',pattern = "HA0[1-2].Rmd")
 #'  
 #'  
-mergeRMD = function(dir = ".",title=".", files, mergedFileName = "book.Rmd") {
+mergeRMD = function(dir = ".",title=".", files, printpaths=TRUE, mergedFileName = "book.Rmd") {
        book_header = paste("---\ntitle:",title, "\n---")
        old = setwd(dir)
        
@@ -28,40 +28,90 @@ mergeRMD = function(dir = ".",title=".", files, mergedFileName = "book.Rmd") {
        } 
        write(book_header, file = mergedFileName)
        
+       # _files_ variable to be able to access it from different Rmd files
+       
+            
        # Introduce Aufgabe names
        task.names=paste0("##Aufgabe ", 1:length(files))
-
-
+       
+       # Introduce a (character) vector of baseDir addresses 
+       dir.insert=character(length=length(files))
+       
+       
+       # Introduce file names. 
+ 
+     
+                
+#      list(matrix(nrow=1,ncol=))
+               
+       
+       # dir.insert=list(matrix(nrow=1,ncol=length(files),dimnames=NULL))
+       
        
        for(i in 1:length(files)){
              text = readLines(files[i])
-             hspan = grep("---", text) # find the boundaries of meta-text
-             # TODO tolk pls invetigate if '---' is allowed in Rmd outside meta text
-             
-             # TODO: tolk pls make the baseDir pointing to the directory the file came from. Before the main text comes
-             
-             
+             metaspan = grep("---", text) # find the boundaries of meta-text
+# TODO tolk pls invetigate if '---' is allowed in Rmd outside meta text
+# Dashes in the text do not affect the outcome, since for each aufgabe, we take the lines between metaspan[1] and metaspan[2].        
+
              # Automatic Name Assignment to Aufgaben
              # Insert Aufgabe titles right after the meta-text
+
+             # Define insert variable to write 'precode' on top of the final document 
+             # (will be erased after each cycle)
+             cell.insert=character(length=1)
              
-             #DONE TODO tolk make option to print out filepathe for each aufgabe after aufgabe name
-             gregexpr(pattern ='/',files[1])[1]
+             # Print Aufgabe Names
+             cell.insert[1]=task.names[i]
              
-             text=c(text[1:hspan[2]],task.names[i],files[i],text[(hspan[2]+1):length(text)])
-             text = text[-c(hspan[1]:hspan[2])] # get rid of the meta-text
+             cell.insert[2]="```{r, echo=FALSE, eval=TRUE,comment=NA} " # comment=NA is to hide ## in knitr output 
+             
+             # Write files variable in the final Rmd document to make it accessable for every r chunk
+             cell.insert[3]="options(useFancyQuotes = FALSE)" # options for sQuote to print plain quotation marks
+             cell.insert[4]=paste("files<<-c(",paste(sQuote(noquote(files)),collapse=","),")")
+
+             # cell.insert[4]=paste("files<<-c(",paste(files,collapse=","),")")
+             
+# DONE TODO: tolk pls make the baseDir in each aufgabe pointing to the directory the file came from.             
+             # change baseDir for every individual Aufgabe
+             cell.insert[5]=paste("baseDir=dirname(files[", i , "])") 
+             
+# DONE TODO: tolk make option to print out filepath for each aufgabe after aufgabe name            
+             # Print Aufgabe file paths depending on the printpaths variable (default is print)
+             if (printpaths==T){
+                #cell.insert[6]=paste("cat(format(files[",i,"]))")
+               
+                cell.insert[6]=paste("message(files[",i,"])")
+             } else {
+                cell.insert[6]=""
+             }
+             
+             cell.insert[7]="```"
+             
+             #  dir.insert[i]= as.character(cat("`r baseDir=dirname(files[",i,"])`", sep=" ")) # cat() gives 0-length output...(?)
+                       
+             text = c(text[1:metaspan[2]],cell.insert[1:7],text[(metaspan[2]+1):length(text)])
+                       
+                      
+             # text = c(text[1:metaspan[2]],task.names[i],files[i],dir.insert[i],text[(metaspan[2]+1):length(text)])
+             text = text[-c(metaspan[1]:metaspan[2])] # get rid of the meta-text
              
              # Delete all variables except lsg and baseDir 
              # to avoid variable conflict across files
              text[length(text)+1]="```{r, echo=FALSE, eval=TRUE}"
              text[length(text)+1]="rm(list=setdiff(objects(),c('lsg','baseDir')))"
              
-             #DONE TODO: tolk, dueo remove or check why package not availibe
+#DONE TODO: tolk, dueo remove or check why package not available
              #  Alternative from package gdata:            
              # text[length(text)+1]="keep(lsg,baseDir,sure=T)"
            
-             text[length(text)+1]="```"                  
+             text[length(text)+1]="```"      
+             
              write(text, sep = "\n", file = mergedFileName, append = T) # sep = "\n" for newline sepation
          }
+
+
+
          # Render the input file to the specified output format using pandoc        
          # render("book.Rmd", output_format = "pdf_document")
          setwd(old)
--- R/Testing.R
+++ R/Testing.R
@@ -3,11 +3,16 @@
 # install_github("Oliver4242/idpedu")
 # library(idpedu)
 
+files <<- c("V:/tolk/Private/PROJECT 02 O. DUERR/TEST FILES 2/HA01.Rmd", "V:/tolk/Private/PROJECT 02 O. DUERR/TEST FILES 2/HA02.Rmd")
+
+
 
 if (FALSE) {
   mergeRMD(dir = "V:/tolk/Private/PROJECT 02 O. DUERR/TEST FILES 2", 
                 title = "Test", files = c("V:/tolk/Private/PROJECT 02 O. DUERR/TEST FILES 2/HA01.Rmd", 
                                           "V:/tolk/Private/PROJECT 02 O. DUERR/TEST FILES 2/HA02.Rmd"), mergedFileName = "book1.Rmd")
+  
   createAB("V:/tolk/Private/PROJECT 02 O. DUERR/TEST FILES 2/book1.Rmd")
 }
+
 
